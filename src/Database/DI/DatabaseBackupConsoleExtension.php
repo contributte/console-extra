4ef6eb6b7890c2b33c450ce78b9feb114fa3087c
Marek Bartoš <bartos.developer152@gmail.com>
2019-06-08 21:05:25 +0200

    Nette 3

--- src/Database/DI/DatabaseBackupConsoleExtension.php
+++ src/Database/DI/DatabaseBackupConsoleExtension.php
@@ -5,32 +5,36 @@ namespace Contributte\Console\Extra\Database\DI;
 use Contributte\Console\Extra\Database\Command\BackupCommand;
 use Contributte\Console\Extra\Database\Command\LoadCommand;
 use Nette\DI\CompilerExtension;
-use Nette\DI\Helpers;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
+use stdClass;
 
+/**
+ * @property-read stdClass $config
+ */
 class DatabaseBackupConsoleExtension extends CompilerExtension
 {
 
-	/** @var mixed[] */
-	private $defaults = [
-		'backupPath' => null,
-		'consoleMode' => false,
-	];
+	public function getConfigSchema(): Schema
+	{
+		return Expect::structure([
+			'backupPath' => Expect::string()->required(),
+			'consoleMode' => Expect::bool(false),
+		]);
+	}
 
 	public function loadConfiguration(): void
 	{
-		$config = $this->validateConfig($this->defaults);
+		$builder = $this->getContainerBuilder();
+		$config = $this->config;
 
 		// Skip if it's not CLI mode
-		if (!$config['consoleMode']) {
+		if (!$config->consoleMode) {
 			return;
 		}
 
-		$builder = $this->getContainerBuilder();
-		$config = $this->validateConfig($this->defaults);
-		$config = Helpers::expand($config, $builder->parameters);
-
 		$builder->addDefinition($this->prefix('backupCommand'))
-			->setFactory(BackupCommand::class, [$config['backupPath']])
+			->setFactory(BackupCommand::class, [$config->backupPath])
 			->setAutowired(false);
 
 		$builder->addDefinition($this->prefix('loadCommand'))
